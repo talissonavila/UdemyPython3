ec34cea2f7a74b708cc5267a416f9bf6ffb3c747
Talisson Avila <talisson_avila@hotmail.com>
2023-08-26 20:07:16 -0300

    add CREATE SQL in CRUD

--- pastas/aula206/main.py
+++ pastas/aula206/main.py
@@ -3,6 +3,8 @@ import dotenv
 import os
 
 
+TABLE_NAME = 'customers'
+
 dotenv.load_dotenv()
 
 connection = pymysql.connect(
@@ -10,8 +12,27 @@ connection = pymysql.connect(
     user=os.environ['MYSQL_USER'],
     password=os.environ['MYSQL_PASSWORD'],
     database=os.environ['MYSQL_DATABASE'],
+    charset='utf8mb4',
 )
+
 with connection:
     with connection.cursor() as cursor:
-        pass
         # SQL
+        cursor.execute(
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+            'id INT NOT NULL AUTO_INCREMENT, '
+            'nome VARCHAR(50) NOT NULL, '
+            'idade INT NOT NULL, '
+            'PRIMARY KEY (id) '
+            ') '
+        )
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')  # Esse comando limpa a tabela inteira
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        results = cursor.execute(
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUE ("Luiz", 25) '
+        )
+        print(f'Resultados = {results}')
+    connection.commit()
