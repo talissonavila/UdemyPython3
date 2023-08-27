4acaa1c2291d87402521511c0a2d537e1db77916
Talisson Avila <talisson_avila@hotmail.com>
2023-08-26 21:02:51 -0300

    insert sql command

--- pastas/aula206/main.py
+++ pastas/aula206/main.py
@@ -29,10 +29,73 @@ with connection:
         cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')  # Esse comando limpa a tabela inteira
     connection.commit()
 
+    # Execute with tuple
     with connection.cursor() as cursor:
-        results = cursor.execute(
+        sql = (
             f'INSERT INTO {TABLE_NAME} '
-            '(nome, idade) VALUE ("Luiz", 25) '
+            '(nome, idade) '
+            'VALUES '
+            '(%s, %s) '
         )
-        print(f'Resultados = {results}')
+        data = ("Luiz", 20)
+        result = cursor.execute(sql, data)
+
+        print(f'Comando sql = {sql}. Data = {data}')
+        print(f'Resultados = {result}')
+    connection.commit()
+
+    # Execute command with dict
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data2 = {
+            "age": 27,
+            "name": "Otavio",
+        }
+        result = cursor.execute(sql, data2)
+
+        print(f'\nComando sql = {sql}. Data = {data2}')
+        print(f'Resultados = {result}')
+    connection.commit()
+
+    # Execute many with tuple of dicts
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data3 = (
+            {"age": 40, "name": "Beatrice", },
+            {"age": 3, "name": "Maria", },
+            {"name": "Paul", "age": 34, },
+            {"age": 9, "name": "Nina", },
+        )
+        result = cursor.executemany(sql, data3)
+
+        print(f'\nComando sql = {sql}. Data = {data3}')
+        print(f'Resultados = {result}')
+    connection.commit()
+
+    # Execute many with tuple of tuples
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%s, %s) '
+        )
+        data4 = (
+            ("Siri", 10),
+            ("Cortana", 6),
+        )
+        result = cursor.executemany(sql, data4)
+
+        print(f'\nComando sql = {sql}. Data = {data4}')
+        print(f'Resultados = {result}')
     connection.commit()
